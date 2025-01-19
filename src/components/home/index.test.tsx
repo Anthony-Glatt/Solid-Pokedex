0b30551832aa70ffdfa3d394b314e4a8980da199
Anthony Glatt <anthony.glatt@gmail.com>
2025-01-18 22:48:12 -0800

    Updated package.json and tsconfig for testing plugins

---
+++ src/components/home/index.test.tsx
@@ -0,0 +1,9 @@
+// import {describe, expect, it} from "vitest";
+// import { render } from '@solidjs/testing-library';
+// import Home from './index';
+
+// describe('Home Page', () => {
+//   it('renders', () => {
+//     render(() => <Home />);
+//   })
+// });
