435f9ca2bf731f72469ff44c40714ab0fe4f071f
Anthony Glatt <anthony.glatt@gmail.com>
2025-01-25 23:37:37 -0800

    Added an api call for pokemon forms in order to pull sprites

--- src/api/pokemon/index.ts
+++ src/api/pokemon/index.ts
@@ -12,4 +12,18 @@ const fetchPokemon = async (name: string) => {
   }
 }
 
-export default fetchPokemon;
+const fetchPokemonForm = async (name: string) => {
+  const url = `https://pokeapi.co/api/v2/pokemon-form/${name}/`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+  }
+}
+
+export { fetchPokemon, fetchPokemonForm };
